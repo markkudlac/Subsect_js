e0edcc59bb7b3d9e47c753473cc2319e4b2fdfbd
Mark Kudlac <mark.kudlac@sympatico.ca>
2015-02-08 14:00:29 -0500

    root is client

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  get '/client', to: 'webrtc#client'
+  get '/server', to: 'webrtc#server'
   
   # You can have the root of your site routed with "root"
-  root 'webrtc#server'
+  root 'webrtc#client'
   
 
   # Example of regular route:
