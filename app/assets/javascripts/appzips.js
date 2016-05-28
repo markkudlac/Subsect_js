f52fd64adc2e7c9f2d1c5550039161ea474f984e
Mark Kudlac <mark.kudlac@sympatico.ca>
2016-05-28 11:45:14 -0400

    add alertmodal

--- app/assets/javascripts/appzips.js
+++ app/assets/javascripts/appzips.js
@@ -1,7 +1,7 @@
 
-function sb_alert(str, func){
+function alertmodal(str, func){
   
-	sb_generateAlertHtml();
+	generateAlertHtml();
 	
 	$("#continueop").off("click").removeClass("hidden");
 	$('#modaltitle').removeClass("alert-danger").removeClass("alert-info");
@@ -18,7 +18,7 @@ function sb_alert(str, func){
 }
 
 
-function sb_generateAlertHtml(){
+function generateAlertHtml(){
 	
 	if ($("#alertmodal").length == 0){
 		$('body').append(
@@ -30,29 +30,11 @@ Close</button><button id="continueop" class="btn btn-primary hidden" \
 data-dismiss="modal">Continue</button></div></div></div>/div>'
 		);
 	}
-	
-	/*
-	This is the above line in a more readable format
-	
-<div id="alertmodal" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 id="modaltitle" 
-									class="modal-title alert-danger" style="padding-left: 10px">Alert</h4>
-            </div>
-            <div class="modal-body">
-                <p id="alertmess"></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default"
-									 data-dismiss="modal">Close</button>
-								<button id="continueop" type="button"
-									class="btn btn-primary hidden" data-dismiss="modal">Continue</button>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-	*/
-	
 }
+
+function toast(jqstr, str){
+    
+	var jqobj = $(jqstr);
+    jqobj.popover({content: str, placement: "bottom", triggetr: "manual"}).popover("show");
+    setTimeout(function(){jqobj.popover("destroy")}, 3000);
+} 
--- public/lib/dev_subsect.js
+++ public/lib/dev_subsect.js
@@ -5,6 +5,50 @@ document.writeln('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/b
 document.writeln('<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>');
 document.writeln('<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.min.js"></script>');
 
+
+function alertmodal(str, func){
+  
+	generateAlertHtml();
+	
+	$("#continueop").off("click").removeClass("hidden");
+	$('#modaltitle').removeClass("alert-danger").removeClass("alert-info");
+	if (typeof func !== "function"){
+		$('#modaltitle').text("Alert").addClass("alert-danger");
+		$("#continueop").addClass("hidden")
+	} else {
+		$('#modaltitle').text("Confirm").addClass("alert-info");
+		$("#continueop").one("click", func);
+	}
+	
+  $('#alertmess').text(str)
+  $('#alertmodal').modal('show')
+}
+
+
+function generateAlertHtml(){
+	
+	if ($("#alertmodal").length == 0){
+		$('body').append(
+'<div id="alertmodal" class="modal fade"><div class="modal-dialog"><div class="modal-content"> \
+<div class="modal-header"><h4 id="modaltitle" class="modal-title alert-danger" \
+style="padding-left: 10px">Alert</h4></div><div class="modal-body"><p id="alertmess"></p> \
+</div><div class="modal-footer"><button class="btn btn-default" data-dismiss="modal"> \
+Close</button><button id="continueop" class="btn btn-primary hidden" \
+data-dismiss="modal">Continue</button></div></div></div>/div>'
+		);
+	}
+}	
+
+
+function toast(jqstr, str){
+
+	var jqobj = $(jqstr);
+    jqobj.popover({content: str, placement: "bottom", triggetr: "manual"}).popover("show");
+    setTimeout(function(){jqobj.popover("destroy")}, 3000);
+} 	
+	
+	
+	
 var SUB_GLB = {
 	SYS_DIR: "sys",
 	DB_SYS: "S_",
