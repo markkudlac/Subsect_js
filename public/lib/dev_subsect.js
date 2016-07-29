0363b2b2805d737a4f27032b87c77fb753f5da19
Mark Kudlac <mark.kudlac@sympatico.ca>
2016-07-29 17:50:41 -0400

    add google verify

--- public/lib/dev_subsect.js
+++ public/lib/dev_subsect.js
@@ -1,4 +1,7 @@
 
+
+document.writeln('<meta name="viewport" content="width=device-width, initial-scale=1" />');
+
 document.writeln('<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>');
 document.writeln('<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>');
 document.writeln('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">');
