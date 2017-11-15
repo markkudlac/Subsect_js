5282b6d2edf75586cdc7513274bdcb1d0f891d6e
Mark Kudlac <mark.kudlac@sympatico.ca>
2017-11-15 15:47:13 -0500

    depricate icervers.url

--- app/assets/javascripts/peer.0.3.13.js
+++ app/assets/javascripts/peer.0.3.13.js
@@ -1417,7 +1417,12 @@ Socket.prototype.close = function() {
 module.exports = Socket;
 
 },{"./util":8,"eventemitter3":9}],8:[function(require,module,exports){
-var defaultConfig = {'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }]};
+	/*
+	Mozilla complains this is depricated use 'urls' instead
+	var defaultConfig = {'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }]};
+	*/
+	
+	var defaultConfig = {'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }]};	//MK updated
 var dataCount = 1;
 
 var BinaryPack = require('js-binarypack');
